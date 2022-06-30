8daa68a0e92f7c1a5a03d8b7d6728127d7c3bfe6
Figo Perdana Putra <perdanaputrafigo@gmail.com>
2022-06-30 13:37:39 +0800

    update

--- invoice.php
+++ invoice.php
@@ -24,21 +24,21 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
+<body class="overflow-x-hidden">
     <?php
     require "./templates/navbarr.php";
     ?>
-    <div class="py-16 bg-gradient-to-br from-[#C27D2B] to-[#CEB27C] w-full h-full">
+    <div class="py-16 bg-gradient-to-br from-[#C27D2B] to-[#CEB27C] w-screen h-full min-h-screen">
 
-        <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div class="container mx-auto px-6 text-gray-600 md:px-12 xl:px-6 ">
             <div class="mb-12 space-y-2 text-center">
                 <h2 class="text-3xl text-white font-bold md:text-4xl pt-6">Invoice</h2>
                 <p class="lg:w-6/12 lg:mx-auto text-gray-100">Berikut adalah history modul apa saja yang sudah dibeli atau belum : </p>
             </div>
-            <div class="grid gap-12 lg:grid-cols-2">
-                <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white shadow-xl hover:rounded-2xl">
+            <div class="grid gap-12 2xl:grid-cols-2 lg:grid-cols-1 md:grid-cols-1">
+                <div class="p-1 rounded-xl group md:flex space-x-6 bg-white shadow-xl hover:rounded-2xl">
                     <img src="assets/img/gradit.png" alt="art cover" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
-                    <div class="sm:w-7/12 pl-0 p-5">
+                    <div class="sm:w-7/12 pl-0 p-5 justify-items-center">
                         <div class="space-y-2">
                             <div class="space-y-4">
                                 <h4 class="text-2xl font-semibold text-cyan-900">GradIT</h4>
@@ -46,43 +46,53 @@
                             </div>
                             <div class="flex flex-row justify-between">
                                 <span class="block w-max px-6 border border-green-200 rounded bg-green-100 text-green-600 shadow">Berhasil</span>
-                                <a type="button" data-modal-toggle="defaultModal" class="cursor-pointer text-white bg-gradient-to-br from-[#C27D2B] to-[#CE7F21] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-400 font-medium rounded-lg text-sm px-5 mt-0.5 text-center shadow">Bayar</a>
-
+                                <a type="button" class="cursor-pointer text-white bg-gradient-to-br from-[#C27D2B] to-[#CE7F21] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-400 font-medium rounded-lg text-sm px-5 pt-0.5 text-center shadow">Bayar</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white shadow-xl hover:rounded-2xl">
+                <div class="p-1 rounded-xl group md:flex space-x-6 bg-white shadow-xl hover:rounded-2xl">
                     <img src="assets/img/gradit.png" alt="art cover" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
-                    <div class="sm:w-7/12 pl-0 p-5">
+                    <div class="sm:w-7/12 pl-0 p-5 justify-items-center">
                         <div class="space-y-2">
                             <div class="space-y-4">
                                 <h4 class="text-2xl font-semibold text-cyan-900">GradIT</h4>
                                 <p class="text-gray-600 pb-2">Laborum saepe laudantium in, voluptates ex placeat quo harum aliquam totam, doloribus eum impedit atque! Temporibus...</p>
                             </div>
-                            <span class="block w-max px-6 border border-red-200 rounded bg-red-100 text-red-600 shadow">Gagal</span>
+                            <div class="flex flex-row justify-between">
+                                <span class="block w-max px-6 border border-red-200 rounded bg-red-100 text-red-600 shadow">Gagal</span>
+                                <a type="button" class="cursor-pointer text-white bg-gradient-to-br from-[#C27D2B] to-[#CE7F21] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-400 font-medium rounded-lg text-sm px-5 pt-0.5 text-center shadow">Bayar</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white shadow-xl hover:rounded-2xl">
+                <div class="p-1 rounded-xl group md:flex space-x-6 bg-white shadow-xl hover:rounded-2xl">
                     <img src="assets/img/gradit.png" alt="art cover" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
-                    <div class="sm:w-7/12 pl-0 p-5">
+                    <div class="sm:w-7/12 pl-0 p-5 justify-items-center">
                         <div class="space-y-2">
                             <div class="space-y-4">
                                 <h4 class="text-2xl font-semibold text-cyan-900">GradIT</h4>
                                 <p class="text-gray-600 pb-2">Laborum saepe laudantium in, voluptates ex placeat quo harum aliquam totam, doloribus eum impedit atque! Temporibus...</p>
                             </div>
-                            <span class="block w-max px-6 border border-yellow-200 rounded bg-yellow-100 text-yellow-600 shadow">Pending</span>
+                            <div class="flex flex-row justify-between">
+                                <span class="block w-max px-6 border border-yellow-200 rounded bg-yellow-100 text-yellow-600 shadow">Pending</span>
+                                <a type="button" class="cursor-pointer text-white bg-gradient-to-br from-[#C27D2B] to-[#CE7F21] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-400 font-medium rounded-lg text-sm px-5 pt-0.5 text-center shadow">Bayar</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <?php
-    require_once "templates/footer.php";
-    ?>
+    <footer class="bg-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">GradIT™</a>. All Rights Reserved.
+        </span>
+        <br>
+        <a href="https://flowbite.com" class="flex items-center mb-6 sm:mb-0 mt-2">
+            <img src="assets/logo/logo_lumintu.png" class="mr-3 h-8" alt="GradIT Logo">
+    </footer>
 </body>
+
 
 
 
