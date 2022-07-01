6b8a10632110e0e12b0457f519b132a6892012f9
Figo Perdana Putra <perdanaputrafigo@gmail.com>
2022-07-01 14:28:06 +0800

    update

---
+++ templates/landingModal.php
@@ -0,0 +1,92 @@
+    <!-- Main modal 1 -->
+    <div id="landingModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+        <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Prokidz
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="landingModal">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <style>
+                    .prose h3 {
+                        margin-bottom: 1rem;
+                        padding-bottom: 0.5rem;
+                        border-bottom: 1px solid rgb(243 244 246);
+
+                        /* @apply mb-1 pb-2 border-b border-gray-200; */
+                    }
+
+                    .prose iframe {
+                        width: 100%;
+                        margin-top: 1.5rem;
+                        aspect-ratio: 16 / 9;
+                        border-radius: 0.75rem;
+
+                        /* @apply w-full aspect-video mt-6 rounded-xl; */
+                    }
+                </style>
+
+                <section>
+                    <div class="relative max-w-screen-xl px-4 py-8 mx-auto">
+                        <div>
+                            <h1 class="text-2xl font-bold lg:text-3xl">
+                                Internet of Things
+                            </h1>
+
+                            <p class="mt-1 text-sm text-gray-500">
+                                Curriculum Overview
+                            </p>
+                        </div>
+
+                        <div class="grid gap-8 lg:items-start lg:grid-cols-4">
+                            <div class="lg:col-span-3">
+                                <div class="relative mt-4">
+                                    <img alt="" src="assets/img/logoprokidz.png" class="w-full rounded-xl h-72 lg:h-[360px] object-cover" />
+                                </div>
+                            </div>
+
+                            <div class="lg:top-0 lg:sticky">
+                                <form class="space-y-4 lg:pt-8">
+                                    <div>
+                                        <p class="text-xl font-bold">
+                                            $19.99
+                                        </p>
+                                    </div>
+
+                                    <button type="submit" class="w-full px-6 py-3 text-sm font-bold tracking-wide text-white uppercase bg-[#C49A6C] rounded">
+                                        Add to cart
+                                    </button>
+                                </form>
+                            </div>
+
+                            <div class="lg:col-span-3">
+                                <div class="prose max-w-none pl-8">
+                                    <h3 class="text-black">manuke farhan cilik</h3>
+
+                                    <p>Features : </p>
+
+                                    <ul class="list-disc pl-8">
+                                        <li>Peran Internet of Things (IoT) dalam kehidupan</li>
+                                        <li>Bermain simbol menggunakan sintaksis</li>
+                                        <li>Fundamental Programming untuk anak</li>
+                                        <li>Belajar coding menggunakan CodeGuppy</li>
+                                        <li>Bermain game seputar coding</li>
+                                        <li>Coding menggunakan parameter, koordinat, dan variabel</li>
+                                        <!-- ... -->
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
